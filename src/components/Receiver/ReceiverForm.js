59c204895a0252f1b7b69de0831e445d2cba8504
ankitkumar0111 <ankit324kumar@gmail.com>
2024-01-04 11:47:25 +0530

    New Update

--- src/components/Receiver/ReceiverForm.js
+++ src/components/Receiver/ReceiverForm.js
@@ -4,32 +4,40 @@ import Search from "../Home/Search";
 import SearchIcon from '@mui/icons-material/Search';
 
 const ReceiverForm = () => {
-//     const [data, setData] = useState()
-//    const fetchCode = async (value) => {
-//     const data = await fetch("http://localhost:3000/country");
-//     const json = await data.json();
+    const [input, setInput] = useState()
+    const [results,setResults] = useState()
+    const [selectedResult, setSelectedResult] = useState()
+   const fetchCode = async (value) => {
+    const data = await fetch("http://localhost:3000/country");
+    const json = await data.json();
 
-//     const filteredData = json.filter((country) => {
-//       return (
-//         country.name.toLowerCase().includes(value.toLowerCase())
-//         || country.phone_code.includes(value)
-//         || country.iso2.includes(value)
-//       );
-//     });
-//     setResults(filteredData);
-//     console.log(results);
-//   };
+    const filteredData = json.filter((country) => {
+      return (
+        country.name.toLowerCase().includes(value.toLowerCase())
+        || country.phone_code.includes(value)
+        || country.iso2.includes(value)
+      );
+    });
+    setResults(filteredData);
+    console.log(results);
+  };
 //   const handleChange = (value) => {
 //     setInput(value);
 //     fetchCode(value);
 //     console.log(value);
 //   };
 
-//   const handleChange = (e) => {
-//     const {value} = e.target
-//     setInput(value)
-//     fetchCode(value)
-//   }
+  const handleChange = (e) => {
+    const {value} = e.target
+    setInput(value)
+    fetchCode(value)
+  }
+
+ const handleClick = (result) => {
+    setResults(null)
+    // setSelectedResult(result);
+    setInput(result.phone_code)
+  };
 
   return (
     <div className="receiver-form">
@@ -40,21 +48,39 @@ const ReceiverForm = () => {
         </div>
         <div className="middle-name">
           <label>
-            <input type="checkbox" />I have middle/second last name
+            <input type="checkbox" />I have middle/second last name.
           </label>
         </div>
         <div className="email">
         <input type="text" placeholder="Enter email (optional)"/>
         </div>
         <div className="country-mobile">
+ <div className="code-result">
         <div className="country-code-search">
         {/* <Search/> */}
-        <input type="text" placeholder="Country Code"  required />
+        <input type="text" placeholder="Country Code" value={input} onChange={handleChange} required />
         <SearchIcon className="search-icon" />
+        </div>
+        <div className="result">
+              <div className="result2">
+                {results && results.map((result, id) => {
+                  return (
+                    <div
+                      key={result.id}
+                      className="result-list"
+                      onClick={() => handleClick(result)}
+                    >
+                      <p>
+                        +{result.phone_code} ({result.iso2})
+                      </p>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
         </div>
         <input className="mobile-number" type="number" placeholder="Mobile number" required />
         </div>
-        
       </form>
     </div>
   );
