a222b1637c93965f09b8c1afc62c7195131a9b16
ankitkumar0111 <ankit324kumar@gmail.com>
2024-02-04 14:59:37 +0530

    Responsive Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,45 @@
-import React from 'react'
-import "./Navbar.css"
 
-const Navbar = () => {
-  return (
-    <div className='navbar'>
-      
-      <div style={{width: "45%"}}>
-         <img className='logo' src="https://www.dollargeneral.com/content/dam/dg/assets/header-navigation/desktoplogo.png" alt="logo-pic" />
-         </div>
-         <div className='navbar-div'>
-         <ul>
-          <li><a href='/home'>Home</a></li>
-          <li><a href='/home'>Find Locations</a></li>
-         </ul>
-         </div>
-    </div>
-  )
+import MenuIcon from '@mui/icons-material/Menu';
+import './Navbar.css';
+import { useState } from 'react';
+ 
+ 
+function Header() {
+ 
+    const [isNavbarOpen, setNavbarOpen] = useState(false)
+ 
+    const toggleNavbar = () => {
+        setNavbarOpen(!isNavbarOpen);
+    }
+    return (
+        <div className='nav-container'>
+            <div className='nav-container-inner-div'>
+                <div className='image-logo-div'>
+                    <img src="" alt='wu-logo' className='image-header' />
+                </div>
+                <div className="vl"></div>
+                <div className='dollar-genenral-div'>
+                    <img src="" alt='dollar-general logo' className='dollar-general-header' />
+                </div>
+ 
+            </div>
+            <div className='nav-content-div'>
+                <p className='home-text-para'>Home</p>
+                <p className='find-locations-para'>Find locations</p>
+            </div>
+            <div className='wu-small-logo-div'>
+                <img src="" alt='logo' className='wu-small-logo' />
+            </div>
+            <div className='menu-item-div' onClick={toggleNavbar}>
+                <MenuIcon className='menu-icon-header' />
+                <div className={`${isNavbarOpen ? 'navlink-open' : 'navbar-close'}`} >
+                    <p>Home</p>
+                    <p>Find locations</p>
+                </div>
+            </div>
+ 
+        </div>
+    );
+ 
 }
-
-export default Navbar
+export default Header;
